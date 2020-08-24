52780a213a943c5e7d4341fceee4a59714914a4b
XxCtrlZxX <harryjung1001@gmail.com>
2020-08-24 21:48:27 +0900

    TODO: MorseInputUtil bug fix

--- app/src/main/java/com/example/morselock2020/MainActivity.java
+++ app/src/main/java/com/example/morselock2020/MainActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
+import com.example.morselock2020.Util.MorseInputUtil;
 import com.example.morselock2020.Util.SaveSharedPreference;
 import com.example.morselock2020.databinding.ActivityMainBinding;
 
@@ -14,25 +19,74 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
-    private float pressTime;
+    private MorseInputUtil morseInputUtil;
+    private float sensitivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        pressTime = SaveSharedPreference.getPrefTime(MainActivity.this);
-        binding.setSensitivity(pressTime);
 
-        binding.timeSeekBar.setProgress((int)(pressTime * 500));
+        sensitivity = SaveSharedPreference.getPrefTime(MainActivity.this);
+        binding.setSensitivity(sensitivity);
+        binding.setIsValid(false);
+        binding.setIsCanReset(false);
+
+        binding.morseInputTxt.setClickable(false);
+        binding.morseInputTxt.setFocusable(false);  // EditText 비활성화
+
+
+        // InputButton
+        CreateInstance();
+        binding.morseInputBtn.setOnTouchListener(onButtonTouchListener);
+
+
+        // ResetButton
+        binding.resetBtn.setOnClickListener(v -> binding.morseInputTxt.setText(""));
+
+
+        // SettingButton
+        binding.settingBtn.setOnClickListener(v -> Toast.makeText(this, "설정 화면으로 넘어갑니다.", Toast.LENGTH_SHORT).show());
+
+
+        // TextChangeListener
+        binding.morseInputTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int len = binding.morseInputTxt.getText().length();
+                if (len >= 4)
+                    binding.setIsValid(true);
+                if (len > 0)
+                    binding.setIsCanReset(true);
+                else {
+                    binding.setIsValid(false);
+                    binding.setIsCanReset(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
         // SeekBar
+        binding.timeSeekBar.setProgress((int)(sensitivity * 500));
         binding.timeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                pressTime = (float)progress / 500;
-                if (pressTime > 0)
-                    binding.timeTxt.setText(String.format("%.2f", pressTime) + "s");
+                sensitivity = (float)progress / 500;
+                if (sensitivity > 0)
+                    binding.timeTxt.setText(String.format("%.2f", sensitivity) + "s");
                 else
                     binding.timeTxt.setText("Disabled");
+                CreateInstance();
             }
 
             @Override
@@ -42,8 +96,30 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                SaveSharedPreference.setPressTime(MainActivity.this, pressTime);
+                SaveSharedPreference.setPressTime(MainActivity.this, sensitivity);
             }
         });
     }
+
+    private void CreateInstance() {
+        if (sensitivity > 0)
+            morseInputUtil = new MorseInputUtil(binding, sensitivity);
+        else
+            morseInputUtil = new MorseInputUtil(binding);
+    }
+
+    private View.OnTouchListener onButtonTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    morseInputUtil.OnMorseBtnDown();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    morseInputUtil.OnMorseBtnUp();
+                    break;
+            }
+            return false;
+        }
+    };
 }
---
+++ app/src/main/java/com/example/morselock2020/Util/MorseInputUtil.java
@@ -0,0 +1,80 @@
+package com.example.morselock2020.Util;
+
+import com.example.morselock2020.databinding.ActivityMainBinding;
+
+public class MorseInputUtil {
+
+    private ActivityMainBinding mainBinding;
+
+    private boolean isBtnDown, isSensitivity, isCanAppend;
+    private float pressTime, sensitivity;
+    private String morse = "";
+
+    //-------- Constructors ---------//
+    public MorseInputUtil(ActivityMainBinding binding) {
+        mainBinding = binding;
+        isBtnDown = false;
+        isSensitivity = false;
+        pressTime = 0;
+    }
+    public MorseInputUtil(ActivityMainBinding binding, float time) {
+        this(binding);
+        isSensitivity = true;
+        sensitivity = time;
+    }
+
+    //------------ Entering Morse Code --------------//
+    private void TimerStart() {
+        TouchThread kThread = new TouchThread();
+        kThread.start();
+    }
+
+    private class TouchThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            while(isBtnDown)
+            {
+                try {
+                    Thread.sleep(5);
+                    pressTime += 0.005;
+
+                    //TODO: 너무 길게 누르면 에러남
+                    if (isSensitivity && pressTime >= sensitivity) {
+                        //morse.append("-");
+                        appendDash(mainBinding);
+                        isCanAppend = false;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //------- public function --------//
+    public void OnMorseBtnDown() {
+        TimerStart();
+        isBtnDown = true;
+        isCanAppend = true;
+        //mainBinding.morseInputTxt.setText("안녕");
+    }
+
+    public void OnMorseBtnUp() {
+        isBtnDown = false;
+        if (isCanAppend) {
+            //morse.append("·");
+            appendDot(mainBinding);
+        }
+    }
+
+    //-------- setText function ----------//
+    private void appendDot(ActivityMainBinding binding) {
+        binding.morseInputTxt.append("·");
+    }
+
+    private void appendDash(ActivityMainBinding binding) {
+        binding.morseInputTxt.append("-");
+    }
+}
